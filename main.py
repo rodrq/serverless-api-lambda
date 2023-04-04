c9ce1d186ec57972b77c763a269857c1e2070955
Rodri Rooney <rodrqq@gmail.com>
2023-04-03 21:21:21 -0300

    Formatting

--- main.py
+++ main.py
@@ -11,20 +11,22 @@ table = dynamodb.Table('your_table_name')
 async def root():
     return {"message": "Hello world"}
 
-@app.get("/get-fare/{day}") #Hour optional
-async def get_fare(day: str, hour: str = None):
-    if hour:
-        response = table.query(
-            KeyConditionExpression=Key('date').eq(day) & Key('time').eq(hour)
-        )
-    else:
-        response = table.query(
-            KeyConditionExpression=Key('date').eq(day)
-        )
+@app.get("/get-fare/{day}") #Gets all fares from day
+async def get_fare(day: str):
+    response = table.query(KeyConditionExpression=Key('date').eq(day))
     items = response.get("Items")
     if not items:
         raise HTTPException(status_code=404, detail=f"Fare not found for {day} at {hour}")
     return items
 
+@app.get("/get-fare/{day}/{hour}") #Gets certain hour fare
+async def get_fare(day: str, hour: str):
+    response = table.query(
+            KeyConditionExpression=Key('date').eq(day) & Key('time').eq(hour)
+        )
+    items = response.get("Items")
+    if not items:
+        raise HTTPException(status_code=404, detail=f"Fare not found for {day} at {hour}")
+    return items
 
 handler = Mangum(app)
